f0e04d7ad50ca3a7a8c460969c1e9b7bd65b91f4
latifaoush <Latifafowh09@gmail.com>
2024-02-29 16:17:09 +0700

    Update penambahan file baru HomeController.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <!-- The whole future lies in uncertainty: live immediately. - Seneca -->
+    Selamat datang di home {{$nama}}<br>
+    Anda adalah seorang {{$pekerjaan}}
+    
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/user/{id}', function ($id) {
+    return 'User dengan ID ' . $id;
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return 'Admin Dashboard';
+    });
+
+    Route::get('/users', function () {
+        return 'Admin Users';
+    });
+});    
+
+Route::get('/listbarang/{id}/{nama}', function($id, $nama){
+    return view('list_barang', compact('id', 'nama'));
+});
+
+
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/contact', [HomeController::class, 'contact']);
